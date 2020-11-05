950bfa471bc87da20d7de18e1929748171300030
Phillip Nguyen <nguye208@gmail.com>
2020-11-04 23:20:45 -0500

    Update ESP8265CapacitiveSoilSensor.ino

--- ESP8265CapacitiveSoilSensor.ino
+++ ESP8265CapacitiveSoilSensor.ino
@@ -5,15 +5,20 @@ const int LOGGER_CHANNEL  = 115200;
 
 const char* ssid     = "";         // The SSID (name) of the Wi-Fi network you want to connect to
 const char* password = "";     // The password of the Wi-Fi network
-const String localApiEndpoint = "http://backend_url/soilmoisture";
-const String httpVariableValue  = "?value=";
-#define sleepDuration           = 3600 //seconds
+
+const String localApiEndpoint  = "http://backend_url/soilmoisture";
+const String httpVariableValue  = "value=";
+#define sleepDuration 60 //seconds
+
+
+HTTPClient http;
+WiFiClient client;
 
 void setup() {
   setupLogger(LOGGER_CHANNEL);
   connectWifi(ssid, password);
   makeSensorHttpCall();
-  ESP.deepSleep(sleepDuration * 1000000, WAKE_RF_DEFAULT);
+  ESP.deepSleep(sleepDuration * 1000000);
 }
 
 
@@ -28,7 +33,8 @@ void setupLogger(int loggerChannel) {
 void connectWifi(String user, String pass) {
   WiFi.begin(user, pass);             // Connect to the network
   Serial.print("Connecting to ");
-  Serial.print(user); Serial.println(" ...");
+  Serial.print(user); 
+  Serial.println(" ...");
 
   int i = 0;
   while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
@@ -43,21 +49,21 @@ void connectWifi(String user, String pass) {
 }
 
 void makeSensorHttpCall() {
-  int value;
-  HTTPClient client;  
+  Serial.println('\n');
+  Serial.println("Starting http call!");  
+  int val;
   
-  value = analogRead(0); //connect sensor to Analog 0
-  Serial.println("The value of the Soil Sensor = " + value); //print the value to serial port
+  val = analogRead(A0); //connect sensor to Analog 0
+  Serial.println(val); //print the value to serial port
 
   // Make a HTTP request:
-  client.begin(localApiEndpoint + httpVariableValue + value);
-
-  int code = client.POST();
+  http.begin(client, localApiEndpoint + "?" + httpVariableValue + val);
+  int code = http.POST(" ");
 
   if (code > 0) {
-    String payload = client.getString();
+    String payload = http.getString();
     Serial.println(payload);
   }
-
-  client.end();
+  http.end();
+  Serial.println("Finished making the HTTP Call");
 }
