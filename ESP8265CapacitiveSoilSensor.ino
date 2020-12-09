4d072b1d441111537cafff25011f56d26265b31c
Phillip Nguyen <nguye208@gmail.com>
2020-12-08 23:12:25 -0500

    Update ESP8265CapacitiveSoilSensor.ino

--- ESP8265CapacitiveSoilSensor.ino
+++ ESP8265CapacitiveSoilSensor.ino
@@ -1,73 +1,84 @@
-#include <ESP8266WiFi.h>        // Include the Wi-Fi library
-#include <ESP8266HTTPClient.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */ 
+#define TIME_TO_SLEEP 5 /* Time ESP32 will go to sleep (in seconds) */
+
 
 const int LOGGER_CHANNEL  = 115200;
 
-const char* ssid     = "";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "";     // The password of the Wi-Fi network
+const char* ssid     = "wifi name";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "wifi password";     // The password of the Wi-Fi network
 
-const String localApiEndpoint  = "http://backend_url/soilmoisture";
-const String httpVariableValue  = "value=";
+const String sensorName       = "Your Sensor Name";
+const String localApiEndpoint  = "http://yourbackend/soilmoisture";
 
-HTTPClient http;
-WiFiClient client;
 
 void setup() {
-  setupLogger(LOGGER_CHANNEL);
-  connectWifi(ssid, password);
-  makeSensorHttpCall();
-  Serial.println("!!!Going to sleep for seconds = " );
-  Serial.println(3600);
-  ESP.deepSleep(3600e6);
-  Serial.println("Does it do anything after deep sleep");
+  setupSerial();
+  setupWifiConnection();
+  //checkingPins();
+  generateResponseBody();
+  uploadSensorData();
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  esp_deep_sleep_start();
 }
 
 
 void loop() {
 }
 
-void setupLogger(int loggerChannel) {
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
-  Serial.println('\n');
-  while(!Serial) { }
+void setupSerial() {
+  Serial.begin(LOGGER_CHANNEL);
 }
 
-void connectWifi(String user, String pass) {
-  WiFi.begin(user, pass);             // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(user); 
-  Serial.println(" ...");
-
-  int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-    delay(1000);
-    Serial.print(++i); Serial.print(' ');
-  }
-
-  Serial.println('\n');
-  Serial.println("Connection established!");  
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
-}
-
-void makeSensorHttpCall() {
-  Serial.println('\n');
-  Serial.println("Starting http call!");  
-  int val;
+void checkingPins() {
+  Serial.printf("Printing the A0: ");
+  Serial.println(analogRead(A0));
   
-  val = analogRead(A0); //connect sensor to Analog 0
-  Serial.println(val); //print the value to serial port
+  Serial.printf("Printing the A1: ");
+  Serial.println(analogRead(A1));
+  
+  Serial.printf("Printing the A2: ");
+  Serial.println(analogRead(A2));
+  
+  Serial.printf("Printing the A3: ");
+  Serial.println(analogRead(A3));
+  
+  Serial.printf("Printing the A4: ");
+  Serial.println(analogRead(A4));
+  
+  Serial.printf("Printing the A5: ");
+  Serial.println(analogRead(A5));
+}
 
-  // Make a HTTP request:
-  http.begin(client, localApiEndpoint + "?" + httpVariableValue + val);
-  int code = http.POST(" ");
-
-  if (code > 0) {
-    String payload = http.getString();
-    Serial.println(payload);
+void setupWifiConnection() {
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
   }
-  http.end();
-  client.stop();
-  Serial.println("Finished making the HTTP Call");
+  Serial.println(" CONNECTED");
+}
 
+void uploadSensorData() {
+  HTTPClient http;
+  http.begin(localApiEndpoint);
+  http.addHeader("Content-Type", "application/json");
+  String body = generateResponseBody();
+//  Serial.printf("Printing the body : ");
+//  Serial.println(body);
+  int httpResponseCode = http.POST(body);
+
+  http.end();
+}
+
+
+String generateResponseBody() {
+  String body = "{\"name\":\"";
+  body += sensorName;
+  body += "\", \"value\":" ;
+  body += analogRead(A2);
+  body += "}";
+  return body;
 }
